702431817822f77c27912c803a378c88a132254b
smarble <smarble@mcad.edu>
2017-10-19 16:12:50 -0500

    8.2 changes

--- exercises/week8-writing-functional-javascript/8.2-theBestThatJSCanBe/exercise-8.2/script.js
+++ exercises/week8-writing-functional-javascript/8.2-theBestThatJSCanBe/exercise-8.2/script.js
@@ -1,8 +1,7 @@
-/* Exercise 8.1
-Go on to the discussion board titled "Week 8 - Exercise 8.1" and create a new Thread. Make sure your title includes the phrase
-“Exercise 8.1”. You must post to the discussion board to receive full participation credit for the week.
+/* Exercise 8.2
+Go on to the discussion board titled "Week 8 - Exercise 8.2" and create a new Thread. Make sure your title includes the phrase “Exercise 8.2”. You must post to the discussion board to receive full participation credit for the week.
+Write one readable and commented JavaScript function and use it to change text in a DIV on an HTML page. Post your code to the new thread.
 
-Create a function that can be reused in different situations on the same HTML page. Post your code to the new thread.
 
 This code is leveraged from video lecture 8.2.5*/
 
@@ -14,15 +13,8 @@ function changeClass(elementID, newClass){
   element. setAttribute("class", newClass); /*for most browsers*/
 }
 
-// using the changeClass function to change the background color of "box1"
+// using the changeClass function to change the background color of "box1" and to change the text
 document.getElementById("box1").onmouseover = function (){
-  changeClass("box1", "colorPurpleYellow");
-}
-
- // using the changeClass function to change the background color of "box1"
- // on this block I used an addEventListener because I read online
- // that it is best to avoid using inline event handlers. But it still only
- // works as an onclick, not an onmouseover.
-document.getElementById("box2").addEventListener("mouseover",function(){
-  changeClass("box2", "colorBlueOrange");
-});
+  changeClass("box1", "colorPurpleYellow")
+  document.getElementById("text").innerHTML = "As much wood as a woodchuck could chuck, If a woodchuck could chuck wood.";
+};
