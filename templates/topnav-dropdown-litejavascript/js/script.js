d665462fc82b55ebdc0b5fd0dc9ea4f02d2940b3
smarble <hellosandramarble@gmail.com>
2018-05-09 14:40:51 -0500

    s

--- templates/topnav-dropdown-litejavascript/js/script.js
+++ templates/topnav-dropdown-litejavascript/js/script.js
@@ -1,3 +1,5 @@
+ // https://www.w3schools.com/howto/howto_js_topnav_responsive.asp  How To-Responsive Top navigation 
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
