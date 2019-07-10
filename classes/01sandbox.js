6557d1112c1e991aad4dae24c4b2fcdf399c6e37
Sandra Marble <hellosandramarble@gmail.com>
2019-07-09 20:50:55 -0500

    After Mike connected Git to Webstorm

--- classes/01sandbox.js
+++ classes/01sandbox.js
@@ -1,75 +1,18 @@
-const menu = {
-    _courses: {
-        appetizers:[] ,
-        mains:[] ,
-        desserts:[] ,
-    },
-    get appetizers(){
-        return this._courses.appetizers;
-    },
-    get mains(){
-        return this._courses.mains;
-    },
-    get desserts(){
-        return this._courses.desserts;
-    },
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-    set appetizers(data){
-        this._courses.appetizers = data;
-    },
-    set mains(data){
-        this._courses.mains = data;
-    },
-    set desserts(data){
-        this._courses.desserts = data;
-    },
-    get courses(){
-        //you could just use "return this._courses" here and it would do the same thing ass all the lines below.
-        return {
-            appetizers: this.appetizers,
-            mains: this. mains,
-            desserts: this.desserts,
-        }
-    },
-    addDishToCourse(courseName, dishName, dishPrice) {
-        const dish = {
-            name: dishName,
-            price: dishPrice,
-        };
-        return this._courses[courseName].push(dish);
-    },
-    getRandomDishFromCourse (courseName) {
-        const dishes = this._courses[courseName];
-        const randomIndex = Math.floor(Math.random() * dishes.length);
-        return dishes[randomIndex];
-    },
-    generateRandomMeal() {
-        const appetizer = this.getRandomDishFromCourse('appetizers');
-        const main = this.getRandomDishFromCourse('mains');
-        const dessert = this.getRandomDishFromCourse('desserts');
-        const totalPrice = appetizer.price + main.price + dessert.price;
+function makeDoggy(e) {
+    // Call this extremely useful function on an <img>.
+    // The <img> will become a picture of a doggy.
+    e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
+    e.target.setAttribute('alt', 'doggy');
+}
 
-        return `Your meal is ${appetizer.name}, ${main.name} and ${dessert.name}. The price is $${totalPrice.toFixed(2)}.`;
-    },
+const kitty = (
+    <img
+onClick = {makeDoggy};
+src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg"
+alt="kitty" />
+);
 
-};
-
-
-menu.addDishToCourse('appetizers', 'Ceasar Salad', 4.25);
-menu.addDishToCourse('appetizers', 'Prawn Coctail', 4.25);
-menu.addDishToCourse('appetizers', 'Garlic Bread', 3.50);
-
-menu.addDishToCourse('mains', 'Lasagna', 9.75);
-menu.addDishToCourse('mains', 'Ribeye Steak', 14.95);
-menu.addDishToCourse('mains', 'Fish & Chips', 12.95);
-
-menu.addDishToCourse('desserts', 'Cheese Cake', 4.50);
-menu.addDishToCourse('desserts', 'Creme Brule', 4.25);
-menu.addDishToCourse('desserts', 'Cheese Board', 3.25);
-
-let meal = menu.generateRandomMeal();
-console.log(meal);
-
-let meal2 = menu.generateRandomMeal();
-console.log(meal2);
-
+ReactDOM.render(kitty, document.getElementById('app'));
